802ae16edfb229be52e3c7cfe38281d688a042e0
Samarth Jindal <samarth.jindal2004@gmail.com>
2025-02-25 11:40:21 +0530

    added authentication files

---
+++ frontend/src/auth/auth.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { usePrivy } from "@privy-io/react-auth";
+import Button from "@/components/common-components/button";
+
+export default function Auth() {
+  const { login, logout, user, ready } = usePrivy();
+
+  if (!ready) return <p>Loading...</p>;
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <Button text={"Acc: ..." + user.wallet?.address.slice(38, 42)} />
+          <Button onClick={logout} text="Logout" />
+        </div>
+      ) : (
+        <Button onClick={login} text="Login with Privy" />
+      )}
+    </div>
+  );
+}
